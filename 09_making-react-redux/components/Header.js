ebff13e7946e0cccedcc0c4e6993dfa3f6d23b34
dev-ahad-ali <ahadali.dev@gmail.com>
2024-10-01 22:59:36 +0600

    Update : making react redux added

---
+++ 09_making-react-redux/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import CartIcon from '../assets/cart-icon.svg';
+import { useSelector } from '../react-redux.js';
+
+export default function Header() {
+  const cartItems = useSelector((state) => state.cartItems);
+  const [itemCount, setItemCount] = useState(0);
+
+  useEffect(() => {
+    const count = cartItems?.reduce(
+      (accumulator, currentItem) => accumulator + currentItem.quantity,
+      0
+    );
+    setItemCount(count);
+  }, [cartItems]);
+
+  return (
+    <header>
+      <div className='header-contents'>
+        <h1>
+          <Link to='/'>Shopee</Link>
+        </h1>
+        <Link className='cart-icon' to='/cart'>
+          <img src={CartIcon} alt='cart-icon' />
+          <div className='cart-items-count'>{itemCount}</div>
+        </Link>
+      </div>
+    </header>
+  );
+}
---
+++ 09_making-react-redux/react-redux.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const StoreContext = createContext();
+
+export function Provider({ children, store }) {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => setState(store.getState()));
+  }, [store]);
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch: store.dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+}
+
+export const useDispatch = () => useContext(StoreContext).dispatch;
+
+export const useSelector = (selector) => selector(useContext(StoreContext).state);
